5c946698fef0d6b4cf4955774bb45456a425e21b
atakriti <anwart256@gmail.com>
2022-05-15 13:48:35 +0200

    new and gebrauchtes wagen done

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,7 +2,7 @@ let fText1 = document.querySelector(".floatedText1")
 let fText2 = document.querySelector(".floatedText2")
 fText1.style.display = "block";
 fText2.style.display = "block";
-
+// highlight =================
 let iX1 = () => {
     if (fText1.style.display === "block") {
         fText1.style.display = "none"
@@ -15,5 +15,15 @@ let iX2 = () => {
         fText2.style.display = "none"
     } else {
         fText2.style.display = "block"
+    }
+}
+// angebote ===================
+let imgText = document.querySelector(".img-text")
+imgText.style.display = "block"
+let iX3 = () => {
+    if (imgText.style.display === "block") {
+        imgText.style.display = "none"
+    } else {
+        imgText.style.display = "block"
     }
 }
