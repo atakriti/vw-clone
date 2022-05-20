c25382fd6f454457be3b6966f734de541aa8aafe
atakriti <anwart256@gmail.com>
2022-05-20 23:45:17 +0200

    animation of functions  done

--- src/javascript/menu-info.js
+++ src/javascript/menu-info.js
@@ -5,7 +5,7 @@ let openBtn = () => {
   if (open.style.display === "none") {
       setTimeout(() => (open.style.display = "block"), 100)
       open.style.animationName = "open"
-      open.style.animationDuration = "2s"
+      open.style.animationDuration = "1s"
     //   ======
       ulCon.style.display = "block"
       ulLinks.style.display = "block"
@@ -16,9 +16,9 @@ let openBtn = () => {
 };
 let closeBtn = () => {
   if (open.style.display === "block") {
-      setTimeout(() => (open.style.display = "none"), 1500)
+      setTimeout(() => (open.style.display = "none"), 1000)
       open.style.animationName = "close"
-      open.style.animationDuration = "2s"
+      open.style.animationDuration = "1s"
     //   ========
       menu1.style.display = "none";
 menu2.style.display = "none";
@@ -54,15 +54,15 @@ let list1 = () => {
   if (menu1.style.display === "none") {
       setTimeout(() => (menu1.style.display = "block"), 100)
       menu1.style.animationName = "listOpen"
-      menu1.style.animationDuration = "2s"
+      menu1.style.animationDuration = "1s"
     //   =========
     menu2.style.display = "none";
     menu3.style.display = "none";
     menu4.style.display = "none";
     menu5.style.display = "none";
       menu6.style.display = "none";
-      setTimeout(() => (ulCon.style.display = "none"), 800)
-      setTimeout(() => (ulLinks.style.display = "none"), 800)
+      setTimeout(() => (ulCon.style.display = "none"), 500)
+      setTimeout(() => (ulLinks.style.display = "none"), 500)
     
     
   } else {
@@ -76,15 +76,15 @@ let list2 = () => {
   if (menu2.style.display === "none") {
       setTimeout(() => (menu2.style.display = "block"), 100)
       menu2.style.animationName = "listOpen"
-      menu2.style.animationDuration = "2s"
+      menu2.style.animationDuration = "1s"
     //   =======
     menu1.style.display = "none";
     menu3.style.display = "none";
     menu4.style.display = "none";
     menu5.style.display = "none";
     menu6.style.display = "none";
-    setTimeout(() => (ulCon.style.display = "none"), 800)
-    setTimeout(() => (ulLinks.style.display = "none"), 800)
+    setTimeout(() => (ulCon.style.display = "none"), 500)
+    setTimeout(() => (ulLinks.style.display = "none"), 500)
   } else {
     menu2.style.display = "none";
     // ulCon.style.display = "block"
@@ -96,15 +96,15 @@ let list3 = () => {
   if (menu3.style.display === "none") {
       setTimeout(() => (menu3.style.display = "block"), 100)
       menu3.style.animationName = "listOpen"
-      menu3.style.animationDuration = "2s"
+      menu3.style.animationDuration = "1s"
     //   =======
     menu1.style.display = "none";
     menu2.style.display = "none";
     menu4.style.display = "none";
     menu5.style.display = "none";
     menu6.style.display = "none";
-    setTimeout(() => (ulCon.style.display = "none"), 800)
-    setTimeout(() => (ulLinks.style.display = "none"), 800)
+    setTimeout(() => (ulCon.style.display = "none"), 500)
+    setTimeout(() => (ulLinks.style.display = "none"), 500)
   } else {
     menu3.style.display = "none";
     // ulCon.style.display = "block"
@@ -116,15 +116,15 @@ let list4 = () => {
   if (menu4.style.display === "none") {
       setTimeout(() => (menu4.style.display = "block"), 100)
       menu4.style.animationName = "listOpen"
-      menu4.style.animationDuration = "2s"
+      menu4.style.animationDuration = "1s"
     //   ========
     menu1.style.display = "none";
     menu2.style.display = "none";
     menu3.style.display = "none";
     menu5.style.display = "none";
     menu6.style.display = "none";
-    setTimeout(() => (ulCon.style.display = "none"), 800)
-    setTimeout(() => (ulLinks.style.display = "none"), 800)
+    setTimeout(() => (ulCon.style.display = "none"), 500)
+    setTimeout(() => (ulLinks.style.display = "none"), 500)
   } else {
     menu4.style.display = "none";
     // ulCon.style.display = "block"
@@ -136,15 +136,15 @@ let list5 = () => {
   if (menu5.style.display === "none") {
       setTimeout(() => (menu5.style.display = "block"), 100)
       menu5.style.animationName = "listOpen"
-      menu5.style.animationDuration = "2s"
+      menu5.style.animationDuration = "1s"
     //   ===========
     menu1.style.display = "none";
     menu2.style.display = "none";
     menu3.style.display = "none";
     menu4.style.display = "none";
     menu6.style.display = "none";
-    setTimeout(() => (ulCon.style.display = "none"), 800)
-    setTimeout(() => (ulLinks.style.display = "none"), 800)
+    setTimeout(() => (ulCon.style.display = "none"), 500)
+    setTimeout(() => (ulLinks.style.display = "none"), 500)
   } else {
     menu5.style.display = "none";
     // ulCon.style.display = "block"
@@ -156,15 +156,15 @@ let list6 = () => {
   if (menu6.style.display === "none") {
       setTimeout(() => (menu6.style.display = "block"), 100)
       menu6.style.animationName = "listOpen"
-      menu6.style.animationDuration = "2s"
+      menu6.style.animationDuration = "1s"
     //   ============
     menu1.style.display = "none";
     menu2.style.display = "none";
     menu3.style.display = "none";
     menu4.style.display = "none";
     menu5.style.display = "none";
-    setTimeout(() => (ulCon.style.display = "none"), 800)
-    setTimeout(() => (ulLinks.style.display = "none"), 800)
+    setTimeout(() => (ulCon.style.display = "none"), 500)
+    setTimeout(() => (ulLinks.style.display = "none"), 500)
   } else {
     menu6.style.display = "none";
     // ulCon.style.display = "block"
@@ -174,12 +174,12 @@ let list6 = () => {
 //  ========================== BackBtn ========
 let back1= () => {
     if (menu1.style.display === "block") {
-        setTimeout(() => (menu1.style.display = "none"), 1200)
+        setTimeout(() => (menu1.style.display = "none"), 1000)
         menu1.style.animationName = "back"
-        menu1.style.animationDuration = "2s"
+        menu1.style.animationDuration = "1s"
         // =========
-        setTimeout(() => (ulCon.style.display = "block"), 800)
-    setTimeout(() => (ulLinks.style.display = "block"), 800)
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
     } else {
         menu1.style.display = "block"
         
@@ -187,12 +187,12 @@ let back1= () => {
 }
 let back2 = () => {
     if (menu2.style.display === "block") {
-        setTimeout(() => (menu2.style.display = "none"), 1200)
+        setTimeout(() => (menu2.style.display = "none"), 1000)
         menu2.style.animationName = "back"
-        menu2.style.animationDuration = "2s"
+        menu2.style.animationDuration = "1s"
         // ========
-        setTimeout(() => (ulCon.style.display = "block"), 800)
-    setTimeout(() => (ulLinks.style.display = "block"), 800)
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
     } else {
         menu2.style.display = "block"
         
@@ -200,11 +200,11 @@ let back2 = () => {
 }
 let back3 = () => {
     if (menu3.style.display === "block") {
-        setTimeout(() => (menu3.style.display = "none"), 1200)
+        setTimeout(() => (menu3.style.display = "none"), 1000)
         menu3.style.animationName = "back"
-        menu3.style.animationDuration = "2s"
-        setTimeout(() => (ulCon.style.display = "block"), 800)
-    setTimeout(() => (ulLinks.style.display = "block"), 800)
+        menu3.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
     } else {
         menu3.style.display = "block"
         
@@ -212,11 +212,11 @@ let back3 = () => {
 }
 let back4 = () => {
     if (menu4.style.display === "block") {
-        setTimeout(() => (menu4.style.display = "none"), 1200)
+        setTimeout(() => (menu4.style.display = "none"), 1000)
         menu4.style.animationName = "back"
-        menu4.style.animationDuration = "2s"
-        setTimeout(() => (ulCon.style.display = "block"), 800)
-    setTimeout(() => (ulLinks.style.display = "block"), 800)
+        menu4.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
     } else {
         menu4.style.display = "block"
         
@@ -224,11 +224,11 @@ let back4 = () => {
 }
 let back5 = () => {
     if (menu5.style.display === "block") {
-        setTimeout(() => (menu5.style.display = "none"), 1200)
+        setTimeout(() => (menu5.style.display = "none"), 1000)
         menu5.style.animationName = "back"
-        menu5.style.animationDuration = "2s"
-        setTimeout(() => (ulCon.style.display = "block"), 800)
-    setTimeout(() => (ulLinks.style.display = "block"), 800)
+        menu5.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
     } else {
         menu5.style.display = "block"
         
@@ -236,11 +236,11 @@ let back5 = () => {
 }
 let back6 = () => {
     if (menu6.style.display === "block") {
-        setTimeout(() => (menu6.style.display = "none"), 1200)
+        setTimeout(() => (menu6.style.display = "none"), 1000)
         menu6.style.animationName = "back"
-        menu6.style.animationDuration = "2s"
-        setTimeout(() => (ulCon.style.display = "block"), 800)
-    setTimeout(() => (ulLinks.style.display = "block"), 800)
+        menu6.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
     } else {
         menu6.style.display = "block"
         
