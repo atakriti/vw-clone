3ff4bb2055f334784f85e152f55b41f7285ff373
atakriti <anwart256@gmail.com>
2022-05-21 13:42:49 +0200

    Updates

---
+++ javascript/main.js
@@ -0,0 +1,165 @@
+let fText1 = document.querySelector(".floatedText1")
+let fText2 = document.querySelector(".floatedText2")
+fText1.style.display = "block";
+fText2.style.display = "block";
+// highlight =================
+let iX1 = () => {
+    if (fText1.style.display === "block") {
+        fText1.style.display = "none"
+    } else {
+        fText1.style.display = "block"
+    }
+}
+let iX2 = () => {
+    if (fText2.style.display === "block") {
+        fText2.style.display = "none"
+    } else {
+        fText2.style.display = "block"
+    }
+}
+// angebote ===================
+let imgText = document.querySelector(".img-text")
+imgText.style.display = "block"
+let iX3 = () => {
+    if (imgText.style.display === "block") {
+        imgText.style.display = "none"
+    } else {
+        imgText.style.display = "block"
+    }
+}
+// =================== up down arraw ==================
+let text1 = document.querySelector(".text1")
+let text2 = document.querySelector(".text2")
+let text3 = document.querySelector(".text3")
+let arrowUp1 = document.querySelector(".fa-angle-up")
+let arrowUp2 = document.querySelector(".up2")
+let arrowUp3 = document.querySelector(".up3")
+let arrowDown1 = document.querySelector(".fa-angle-down")
+let arrowDown2 = document.querySelector(".down2")
+let arrowDown3 = document.querySelector(".down3")
+arrowDown1.style.display = "block"
+arrowUp1.style.display = "none"
+text1.style.height = "10%"
+text2.style.height = "10%"
+text3.style.height = "12%"
+// ================== pic 1==========
+let down1 = () => {
+    if (text1.style.height === "10%") {
+        setTimeout(() => (text1.style.height = "29%"), 500)
+        text1.style.animationName = "down1"
+        text1.style.animationDuration = "2s"
+        arrowDown1.style.display = "none"
+        arrowUp1.style.display = "block"
+        arrowUp1.style.backgroundColor = "rgb(34, 20, 124)"
+        arrowUp1.style.color = "white"
+
+    } else {
+        text1.style.height = "10%"
+        arrowDown1.style.display = "block"
+        arrowUp1.style.display = "none"
+    }
+}
+let up1 = () => {
+    if (text1.style.height === "29%") {
+        setTimeout(() => (text1.style.height = "10%"), 500)
+        text1.style.animationName = "up1"
+        text1.style.animationDuration = "2s"
+        arrowDown1.style.display = "block"
+        arrowUp1.style.display = "none"
+        
+    } else {
+        text1.style.height = "29%"
+        arrowDown1.style.display = "none"
+        arrowUp1.style.display = "block"
+    }
+}
+// =============== pic 2=============
+// =============================
+let down2 = () => {
+    if (text2.style.height === "10%") {
+        setTimeout(() => (text2.style.height = "37%"), 500)
+        text2.style.animationName = "down2"
+        text2.style.animationDuration = "2s"
+        arrowDown2.style.display = "none"
+        arrowUp2.style.display = "block"
+        arrowUp2.style.backgroundColor = "rgb(34, 20, 124)"
+        arrowUp2.style.color = "white"
+
+    } else {
+        text2.style.height = "10%"
+        arrowDown2.style.display = "block"
+        arrowUp2.style.display = "none"
+    }
+}
+let up2 = () => {
+    if (text2.style.height === "37%") {
+        setTimeout(() => (text2.style.height = "10%"), 500)
+        text2.style.animationName = "up2"
+        text2.style.animationDuration = "2s"
+        arrowDown2.style.display = "block"
+        arrowUp2.style.display = "none"
+    } else {
+        text2.style.height = "37%"
+        arrowDown2.style.display = "none"
+        arrowUp2.style.display = "block"
+    }
+}
+// ================= pic3 ===========
+let down3 = () => {
+    if (text3.style.height === "12%") {
+        setTimeout(() => (text3.style.height = "45%"), 500)
+        text3.style.animationName = "down3"
+        text3.style.animationDuration = "2s"
+        arrowDown3.style.display = "none"
+        arrowUp3.style.display = "block"
+        arrowUp3.style.backgroundColor = "rgb(34, 20, 124)"
+        arrowUp3.style.color = "white"
+
+    } else {
+        text3.style.height = "12%"
+        arrowDown3.style.display = "block"
+        arrowUp3.style.display = "none"
+    }
+}
+let up3 = () => {
+    if (text3.style.height === "45%") {
+        setTimeout(() => (text3.style.height = "12%"), 500)
+        text3.style.animationName = "up3"
+        text3.style.animationDuration = "2s"
+        arrowDown3.style.display = "block"
+        arrowUp3.style.display = "none"
+    } else {
+        text3.style.height = "45%"
+        arrowDown3.style.display = "none"
+        arrowUp3.style.display = "block"
+    }
+}
+
+// ========================= NeuigKeiten ====================
+let textNeuig1 = document.querySelector(".img-text1")
+let textNeuig2 = document.querySelector(".img-text2")
+let textNeuig3 = document.querySelector(".img-text3")
+textNeuig1.style.display = "block"
+textNeuig2.style.display = "block"
+textNeuig3.style.display = "block"
+let iX4 = () => {
+    if (textNeuig1.style.display === "block") {
+        textNeuig1.style.display = "none"
+    } else {
+        textNeuig1.style.display = "block"
+    }
+}
+let iX5 = () => {
+    if (textNeuig2.style.display === "block") {
+        textNeuig2.style.display = "none"
+    } else {
+        textNeuig2.style.display = "block"
+    }
+}
+let iX6 = () => {
+    if (textNeuig3.style.display === "block") {
+        textNeuig3.style.display = "none"
+    } else {
+        textNeuig3.style.display = "block"
+    }
+}
---
+++ javascript/menu-info.js
@@ -0,0 +1,248 @@
+// the open menu
+let open = document.querySelector(".hamburger");
+open.style.display = "block";
+let openBtn = () => {
+  if (open.style.display === "none") {
+      setTimeout(() => (open.style.display = "block"), 100)
+      open.style.animationName = "open"
+      open.style.animationDuration = "1s"
+    //   ======
+      ulCon.style.display = "block"
+      ulLinks.style.display = "block"
+  } else {
+      open.style.display = "none"
+      
+  }
+};
+let closeBtn = () => {
+  if (open.style.display === "block") {
+      setTimeout(() => (open.style.display = "none"), 500)
+      open.style.animationName = "close"
+      open.style.animationDuration = "0.5s"
+    //   ========
+      menu1.style.display = "none";
+menu2.style.display = "none";
+menu3.style.display = "none";
+menu4.style.display = "none";
+menu5.style.display = "none";
+menu6.style.display = "none";
+  } else {
+    open.style.display = "block";
+  }
+};
+// ==========================
+
+let menu1 = document.querySelector(".menu1");
+let menu2 = document.querySelector(".menu2");
+let menu3 = document.querySelector(".menu3");
+let menu4 = document.querySelector(".menu4");
+let menu5 = document.querySelector(".menu5");
+let menu6 = document.querySelector(".menu6");
+let ulCon = document.querySelector(".container");
+let ulLinks = document.querySelector(".ul-links");
+// =========================== now making the menus none
+menu1.style.display = "none";
+menu2.style.display = "none";
+menu3.style.display = "none";
+menu4.style.display = "none";
+menu5.style.display = "none";
+menu6.style.display = "none";
+ulCon.style.display = "block";
+ulLinks.style.display = "block";
+// ================================= now the conditions ========
+let list1 = () => {
+  if (menu1.style.display === "none") {
+      setTimeout(() => (menu1.style.display = "block"), 100)
+      menu1.style.animationName = "listOpen"
+      menu1.style.animationDuration = "1s"
+    //   =========
+    menu2.style.display = "none";
+    menu3.style.display = "none";
+    menu4.style.display = "none";
+    menu5.style.display = "none";
+      menu6.style.display = "none";
+      setTimeout(() => (ulCon.style.display = "none"), 100)
+      setTimeout(() => (ulLinks.style.display = "none"), 100)
+    
+    
+  } else {
+    menu1.style.display = "none";
+    // ulCon.style.display = "block"
+    // ulLinks.style.display = "block"
+  }
+};
+// ========================================================================
+let list2 = () => {
+  if (menu2.style.display === "none") {
+      setTimeout(() => (menu2.style.display = "block"), 100)
+      menu2.style.animationName = "listOpen"
+      menu2.style.animationDuration = "1s"
+    //   =======
+    menu1.style.display = "none";
+    menu3.style.display = "none";
+    menu4.style.display = "none";
+    menu5.style.display = "none";
+    menu6.style.display = "none";
+    setTimeout(() => (ulCon.style.display = "none"), 100)
+    setTimeout(() => (ulLinks.style.display = "none"), 100)
+  } else {
+    menu2.style.display = "none";
+    // ulCon.style.display = "block"
+    // ulLinks.style.display = "block"
+  }
+};
+// ========================================================================
+let list3 = () => {
+  if (menu3.style.display === "none") {
+      setTimeout(() => (menu3.style.display = "block"), 100)
+      menu3.style.animationName = "listOpen"
+      menu3.style.animationDuration = "1s"
+    //   =======
+    menu1.style.display = "none";
+    menu2.style.display = "none";
+    menu4.style.display = "none";
+    menu5.style.display = "none";
+    menu6.style.display = "none";
+    setTimeout(() => (ulCon.style.display = "none"), 100)
+    setTimeout(() => (ulLinks.style.display = "none"), 100)
+  } else {
+    menu3.style.display = "none";
+    // ulCon.style.display = "block"
+    // ulLinks.style.display = "block"
+  }
+};
+// ========================================================================
+let list4 = () => {
+  if (menu4.style.display === "none") {
+      setTimeout(() => (menu4.style.display = "block"), 100)
+      menu4.style.animationName = "listOpen"
+      menu4.style.animationDuration = "1s"
+    //   ========
+    menu1.style.display = "none";
+    menu2.style.display = "none";
+    menu3.style.display = "none";
+    menu5.style.display = "none";
+    menu6.style.display = "none";
+    setTimeout(() => (ulCon.style.display = "none"), 100)
+    setTimeout(() => (ulLinks.style.display = "none"), 100)
+  } else {
+    menu4.style.display = "none";
+    // ulCon.style.display = "block"
+    // ulLinks.style.display = "block"
+  }
+};
+// ========================================================================
+let list5 = () => {
+  if (menu5.style.display === "none") {
+      setTimeout(() => (menu5.style.display = "block"), 100)
+      menu5.style.animationName = "listOpen"
+      menu5.style.animationDuration = "1s"
+    //   ===========
+    menu1.style.display = "none";
+    menu2.style.display = "none";
+    menu3.style.display = "none";
+    menu4.style.display = "none";
+    menu6.style.display = "none";
+    setTimeout(() => (ulCon.style.display = "none"), 100)
+    setTimeout(() => (ulLinks.style.display = "none"), 100)
+  } else {
+    menu5.style.display = "none";
+    // ulCon.style.display = "block"
+    // ulLinks.style.display = "block"
+  }
+};
+// ========================================================================
+let list6 = () => {
+  if (menu6.style.display === "none") {
+      setTimeout(() => (menu6.style.display = "block"), 100)
+      menu6.style.animationName = "listOpen"
+      menu6.style.animationDuration = "1s"
+    //   ============
+    menu1.style.display = "none";
+    menu2.style.display = "none";
+    menu3.style.display = "none";
+    menu4.style.display = "none";
+    menu5.style.display = "none";
+    setTimeout(() => (ulCon.style.display = "none"), 100)
+    setTimeout(() => (ulLinks.style.display = "none"), 100)
+  } else {
+    menu6.style.display = "none";
+    // ulCon.style.display = "block"
+    // ulLinks.style.display = "block"
+  }
+};
+//  ========================== BackBtn ========
+let back1= () => {
+    if (menu1.style.display === "block") {
+        setTimeout(() => (menu1.style.display = "none"), 1000)
+        menu1.style.animationName = "back"
+        menu1.style.animationDuration = "1s"
+        // =========
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
+    } else {
+        menu1.style.display = "block"
+        
+    }
+}
+let back2 = () => {
+    if (menu2.style.display === "block") {
+        setTimeout(() => (menu2.style.display = "none"), 1000)
+        menu2.style.animationName = "back"
+        menu2.style.animationDuration = "1s"
+        // ========
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
+    } else {
+        menu2.style.display = "block"
+        
+    }
+}
+let back3 = () => {
+    if (menu3.style.display === "block") {
+        setTimeout(() => (menu3.style.display = "none"), 1000)
+        menu3.style.animationName = "back"
+        menu3.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
+    } else {
+        menu3.style.display = "block"
+        
+    }
+}
+let back4 = () => {
+    if (menu4.style.display === "block") {
+        setTimeout(() => (menu4.style.display = "none"), 1000)
+        menu4.style.animationName = "back"
+        menu4.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
+    } else {
+        menu4.style.display = "block"
+        
+    }
+}
+let back5 = () => {
+    if (menu5.style.display === "block") {
+        setTimeout(() => (menu5.style.display = "none"), 1000)
+        menu5.style.animationName = "back"
+        menu5.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
+    } else {
+        menu5.style.display = "block"
+        
+    }
+}
+let back6 = () => {
+    if (menu6.style.display === "block") {
+        setTimeout(() => (menu6.style.display = "none"), 1000)
+        menu6.style.animationName = "back"
+        menu6.style.animationDuration = "1s"
+        setTimeout(() => (ulCon.style.display = "block"), 500)
+    setTimeout(() => (ulLinks.style.display = "block"), 500)
+    } else {
+        menu6.style.display = "block"
+        
+    }
+}
